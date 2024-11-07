bb2d4b6d18c7376563cd2919c8e7b50e1c3e451e
mashkinsa <mariasmolovik1@gmail.com>
2024-11-07 07:29:25 +0700

    second commit

--- catalog/models.py
+++ catalog/models.py
@@ -8,29 +8,31 @@ class Genre(models.Model):
 # Модель, представляющая книжный жанр
     name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
 
-def __str__(self):
-# Строка для представления объекта модели (на сайте администратора и т.д.)
-    return self.name
+    def __str__(self):
+    # Строка для представления объекта модели (на сайте администратора и т.д.)
+        return self.name
 
 class Book(models.Model):
-# Модель, представляющая книгу (но не конкретную копию книги).
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-# Используется внешний ключ, потому что у книги может быть только один автор, но у авторов может быть несколько книг
-# Автор в виде строки, а не объекта, поскольку он еще не был объявлен в файле.
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
 
-# ManyToManyField используется потому, что жанр может содержать много книг. Книги могут охватывать много жанров.
-# Класс жанра уже определен, поэтому мы можем указать объект, указанный выше.
+    display_genre.short_description = 'Genre'
+
     def __str__(self):
-# Строка для представления модельного объекта.
-        return self.title
+    # Строка для представления модельного объекта.
+            return self.title
 
     def get_absolute_url(self):
-# Возвращает URL-адрес для доступа к определенному экземпляру книги.
-        return reverse('book-detail', args=[str(self.id)])
+    # Возвращает URL-адрес для доступа к определенному экземпляру книги.
+            return reverse('book-detail', args=[str(self.id)])
 
 import uuid # Требуется для уникальных экземпляров книг
 
@@ -40,6 +42,9 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    # status = models.CharField(max_length=200, default='доступен')
+
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -49,6 +54,7 @@ class BookInstance(models.Model):
     )
 # CharField, для представления данных (конкретного выпуска) о книге.
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
 
     class Meta:
         ordering = ["due_back"]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,41 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+def index(request):  # Функция отображения для домашней страницы сайта.
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()  # Доступные книги (статус = 'a')
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genre = Genre.objects.all().count()
+    search_word = None
+    if request.method == 'POST':
+        search_word = request.POST.get('search_word', '').strip()
+        # Проверяем, что слово для поиска не пустое
+        if search_word:
+            # Получаем количество книг, содержащих слово в заголовке (без учета регистра)
+            num_books = Book.objects.filter(title__icontains=search_word.lower()).count()
+
+
+    # Отрисовка HTML-шаблона index.html с данными внутри переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors, 'num_genre': num_genre, 'search_word': search_word},
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+
+
+
